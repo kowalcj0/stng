75b913f0fd141ec07c6c9482dc032175de950e99
Janusz Kowalczyk <kowalczykjanusz@gmail.com>
2014-03-19 15:42:32 +0000

    finally configured pom to generate proper HTML JBehave report added duplicated scenarios to the stories so that we can check if they are getting to the report

--- src/test/java/org/stng/jbehave/ModifiedOfficialJBehaveTestNGStories.java
+++ src/test/java/org/stng/jbehave/ModifiedOfficialJBehaveTestNGStories.java
@@ -1,10 +1,13 @@
 package org.stng.jbehave;
 
 import org.apache.log4j.Logger;
+import org.jbehave.core.Embeddable;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.reporters.FilePrintStreamFactory;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
@@ -13,6 +16,7 @@ import org.stng.jbehave.steps.JBehaveFirstStorySteps;
 import org.stng.jbehave.steps.JBehaveSecondStorySteps;
 
 import java.util.List;
+import java.util.Properties;
 
 import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
 
@@ -28,12 +32,24 @@ public class ModifiedOfficialJBehaveTestNGStories extends testNGStories {
     // Here we specify the configuration, starting from default MostUsefulConfiguration, and changing only what is needed
     @Override
     public Configuration configuration() {
+        // partially based upon http://jbehave.org/reference/stable/reporting-stories.html
         log.info("configuration()");
+        Class<? extends Embeddable> embeddableClass = this.getClass();
+        Properties viewResources = new Properties();
+        viewResources.put("decorateNonHtml", "true");
         return new MostUsefulConfiguration()
                 // where to find the stories
                 .useStoryLoader(new LoadFromClasspath(this.getClass()))
                  // CONSOLE and TXT reporting
-                .useStoryReporterBuilder(new StoryReporterBuilder().withDefaultFormats().withFormats(Format.CONSOLE, Format.HTML, Format.XML, Format.TXT));
+                .useStoryReporterBuilder(
+                        new StoryReporterBuilder()
+                                .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                                .withDefaultFormats()
+                                .withPathResolver(new FilePrintStreamFactory.ResolveToPackagedName())
+                                .withViewResources(viewResources)
+                                .withFormats(Format.CONSOLE, Format.HTML, Format.XML, Format.TXT)
+                                .withFailureTrace(true)
+                                .withFailureTraceCompression(true));
     }
 
     // Here we specify the steps classes
